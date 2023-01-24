fdab7c1d046b833eab341637ad6170c71700f1ad
yschristian <yubahwesc@gmail.com>
2023-01-24 19:03:44 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/pages/Navbar"
-import { useState } from "react"
+import { useState, useEffect} from "react"
 import { SelectedPage } from "@/shared/types"
 import Home from "@/pages/Home"
 import RoadSigns from "@/pages/RoadSigns"
@@ -10,11 +10,25 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
 
+  useEffect(() => {
+    const handleScroll = async() =>{
+      if(window.scrollY === 0){
+        setIsTopOfPage(true)
+        setSelectedPage(SelectedPage.Home)
+      }
+      if(window.scrollY !== 0){
+        setIsTopOfPage(false)
+      }
+    }
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
   return (
     <div className="App">
       <Navbar
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
+        isTopOfPage={isTopOfPage}
       />
       <Home
         setSelectedPage={setSelectedPage}
--- src/pages/Navbar/index.tsx
+++ src/pages/Navbar/index.tsx
@@ -8,20 +8,23 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 type Props = {
     selectedPage: SelectedPage,
-    setSelectedPage: (value: SelectedPage) => void
+    setSelectedPage: (value: SelectedPage) => void,
+    isTopOfPage: boolean;
 }
 
 const Navbar = ({
     selectedPage,
-    setSelectedPage
+    setSelectedPage,
+    isTopOfPage
 }: Props) => {
     const flexBetween = "flex items-center justify-between"
     const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
     const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false)
+    const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
 
     return (
         <nav>
-            <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+            <div className={` ${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6`}>
                 <div className={`${flexBetween} mx-auto w-5/6`}>
                     <div className={`${flexBetween} w-full gap-16 `}>
                         <img src={Logo} alt="logo" />
